fb1682b6366f6d20469ab32f578cdd31a0c5648a
alibaba <ppnjy@qq.com>
2021-09-01 12:14:09 +0800

    Removed provider config

--- example/main.tf
+++ example/main.tf
@@ -2,11 +2,6 @@ variable "instance_alias" {
   default = "terraform_test"
 }
 
-provider "alicloud" {
-  region  = "cn-shenzhen"
-  profile = "default"
-}
-
 data "alicloud_zones" "default" {
   enable_details = true
 }
