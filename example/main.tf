4ec06ef7ca52eac9da598382eba6f7b9f852309e
王超 <wc01169074@alibaba-inc.com>
2021-12-07 16:06:12 +0800

    修改zone_id

--- example/main.tf
+++ example/main.tf
@@ -1,25 +1,16 @@
-variable "instance_alias" {
-  default = "terraform_test"
-}
-
-data "alicloud_zones" "default" {
-  enable_details = true
-}
+data "alicloud_tsdb_zones" "default" {}
 
 resource "alicloud_vpc" "default" {
-  vpc_name   = "terraform_test"
-  cidr_block = "172.16.0.0/16"
+  cidr_block = "192.168.0.0/16"
+  vpc_name       = "tf-testaccTsdbInstance"
 }
 
-resource "alicloud_vswitch" "default" {
-  vswitch_name = "terraform_test"
-  cidr_block   = "172.16.0.0/16"
-  vpc_id       = alicloud_vpc.default.id
-  zone_id      = data.alicloud_zones.default.zones.0.id
+resource "alicloud_vswitch" "example" {
+  zone_id = data.alicloud_tsdb_zones.default.ids.0
+  cidr_block        = "192.168.1.0/24"
+  vpc_id            = alicloud_vpc.default.id
 }
-
 module "example" {
   source         = "../"
-  instance_alias = var.instance_alias
-  vswitch_id     = alicloud_vswitch.default.id
+  vswitch_id     = alicloud_vswitch.example.id
 }
